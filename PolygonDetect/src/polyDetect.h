fe510db9d7f40ec16cc91814dce38c0583995613
szt6b6 <34064351+szt6b6@users.noreply.github.com>
2022-12-29 14:09:05 +0800

    多边形端点及边检测

---
+++ PolygonDetect/src/polyDetect.h
@@ -0,0 +1,10 @@
+#include<windows.h>
+#include<iostream>
+#include<opencv2/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
+
+
+using namespace cv;
+using namespace std;
+
+extern"C" __declspec(dllexport) void polygoDetect(Mat& srcImg, double epsilon, int minAcreage, vector<vector<float>>& lines, Mat& dst);
