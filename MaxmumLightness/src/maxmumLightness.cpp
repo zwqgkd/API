fe976b874403ab81395788bc342e4599345a6405
高增光 <1980985772@qq.com>
2022-12-30 03:17:36 +0800

    gzg

---
+++ MaxmumLightness/src/maxmumLightness.cpp
@@ -0,0 +1,14 @@
+#include"precomp.hpp"
+
+using namespace cv;
+
+double meanLightness(cv::Mat img) {
+	cv::Mat img_hls;
+	cv::cvtColor(img, img_hls, CV_BGR2HLS);
+	std::vector<cv::Mat>images;
+	split(img_hls, images);
+	double *maxVal=NULL;
+	cv::minMaxLoc(images[1], NULL, maxVal, NULL, NULL);
+	return *maxVal;
+	
+}
---
+++ MinimumLightness/src/minimumLightness.cpp
@@ -0,0 +1,14 @@
+#include"precomp.hpp"
+
+using namespace cv;
+
+double meanLightness(cv::Mat img) {
+	cv::Mat img_hls;
+	cv::cvtColor(img, img_hls, CV_BGR2HLS);
+	std::vector<cv::Mat>images;
+	split(img_hls, images);
+	double *minVal = NULL;
+	cv::minMaxLoc(images[1], minVal, NULL, NULL, NULL);
+	return *minVal;
+
+}
