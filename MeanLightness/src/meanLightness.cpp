7c3567b752377eb0a088514584d66adf1b84f6a1
高增光 <1980985772@qq.com>
2023-02-03 00:11:44 +0800

    gzg

--- MeanLightness/src/meanLightness.cpp
+++
@@ -1,8 +0,0 @@
-#include"precomp.hpp"
-
-double meanLightness(cv::Mat img) {
-	cv::Mat img_hls;
-	cv::cvtColor(img, img_hls, CV_BGR2HLS);
-	cv::Scalar scalar = cv::mean(img_hls);
-	return scalar.val[1];
-}
