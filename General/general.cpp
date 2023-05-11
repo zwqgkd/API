a1024d169ec1ba6cf288af513b74e15c7f1357bf
orocane <s1ran@outlook.com>
2023-05-11 16:59:16 +0800

    Add flip, resize and crop

--- General/general.cpp
+++ General/general.cpp
@@ -1,19 +1,19 @@
 #include "object.h"
 
-#define EXPORT extern "C" __declspec(dllexport)
+#define __EXPORT extern "C" __declspec(dllexport)
 
-EXPORT void intIO(ParamPtrArray& params) {
+__EXPORT void intIO(ParamPtrArray& params) {
 	int *dst = new int(get_data<int>(params[0]));
 	params.push_back(make_param("dst", "int", dst));
 }
 
-EXPORT void gaussian_blur(ParamPtrArray& params) {
+__EXPORT void gaussian_blur(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	cv::GaussianBlur(get_data<cv::Mat>(params[0]), *dst, get_data<cv::Size>(params[1]), get_data<double>(params[2]), get_data<double>(params[3]), get_data<int>(params[4]));
 	params.push_back(make_param("dst", "Mat", dst));
 }
 
-EXPORT void read(ParamPtrArray& params) {
+__EXPORT void read(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	*dst = cv::imread(
 		get_data<std::string>(params[0]),
@@ -25,7 +25,7 @@ EXPORT void read(ParamPtrArray& params) {
 
 
 
-EXPORT void morph_open(ParamPtrArray& params) {
+__EXPORT void morph_open(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	cv::Mat kernel = cv::getStructuringElement(
 		cv::MORPH_RECT,
@@ -40,7 +40,7 @@ EXPORT void morph_open(ParamPtrArray& params) {
 	params.push_back(make_param("dst","Mat",dst));
 }
 
-EXPORT void morph_close(ParamPtrArray& params) {
+__EXPORT void morph_close(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	cv::Mat kernel = cv::getStructuringElement(
 		cv::MORPH_RECT,
@@ -55,7 +55,7 @@ EXPORT void morph_close(ParamPtrArray& params) {
 	params.push_back(make_param("dst","Mat",dst));
 }
 
-EXPORT void threshold(ParamPtrArray& params) {
+__EXPORT void threshold(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	cv::threshold(
 		get_data_const_ref<cv::Mat>(params[0]),
@@ -67,7 +67,7 @@ EXPORT void threshold(ParamPtrArray& params) {
 	params.push_back(make_param("dst", "Mat", dst));
 }
 
-EXPORT void convert_color(ParamPtrArray& params) {
+__EXPORT void convert_color(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	
 	cv::cvtColor(
@@ -78,7 +78,7 @@ EXPORT void convert_color(ParamPtrArray& params) {
 	params.push_back(make_param("dst", "Mat", dst));
 }
 
-EXPORT void median_blur(ParamPtrArray& params) {
+__EXPORT void median_blur(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	cv::medianBlur(
 		get_data_const_ref<cv::Mat>(params[0]),
@@ -88,7 +88,7 @@ EXPORT void median_blur(ParamPtrArray& params) {
 	params.push_back(make_param("dst", "Mat", dst));
 }
 
-EXPORT void find_contours(ParamPtrArray& params) {
+__EXPORT void find_contours(ParamPtrArray& params) {
 	using ContoursType = std::vector<std::vector<cv::Point>>;
 	ContoursType* contours = new ContoursType;
 	cv::findContours(
@@ -100,7 +100,7 @@ EXPORT void find_contours(ParamPtrArray& params) {
 	params.push_back(make_param("contours","vector<vector<Point>>",contours));
 }
 
-EXPORT void draw_contours(ParamPtrArray& params) {
+__EXPORT void draw_contours(ParamPtrArray& params) {
 	using ContoursType = std::vector<std::vector<cv::Point>>;
 	cv::Mat* dst = new cv::Mat(get_data_const_ref<cv::Mat>(params[0]));
 	cv::drawContours(
@@ -123,7 +123,7 @@ EXPORT void draw_contours(ParamPtrArray& params) {
 *  3. <b><em>output</em></b> A `std::vector<cv::Point>` object representing the selected contour.
 */
 
-EXPORT void select_contour(ParamPtrArray& params) {
+__EXPORT void select_contour(ParamPtrArray& params) {
 	using ContourType = std::vector<cv::Point>;
 	using ContoursType = std::vector<ContourType>;
 
@@ -140,7 +140,7 @@ EXPORT void select_contour(ParamPtrArray& params) {
 *  2. <b><em>output</em></b> A `cv::RotatedRect` object representing the minimum area circumscribing rectangle.
 */
 
-EXPORT void min_area_rect(ParamPtrArray& params) {
+__EXPORT void min_area_rect(ParamPtrArray& params) {
 	using ContourType = std::vector<cv::Point>;
 
 	const ContourType& contour = get_data_const_ref<ContourType>(params[0]);
@@ -155,7 +155,7 @@ EXPORT void min_area_rect(ParamPtrArray& params) {
 *  2. <b><em>output</em></b> A `std::vector<cv::Point>` object representing the 4 vertices.
 */
 
-EXPORT void box_points(ParamPtrArray& params) {
+__EXPORT void box_points(ParamPtrArray& params) {
 	const cv::RotatedRect& rect = get_data_const_ref<cv::RotatedRect>(params[0]);
 	cv::Point2f vertices_array[4];
 	rect.points(vertices_array);
@@ -173,7 +173,7 @@ EXPORT void box_points(ParamPtrArray& params) {
 *  2. <b><em>output</em></b> A `std::vector<std::vector<cv::Point>>` object representing the contours.
 */
 
-EXPORT void contour_to_contours(ParamPtrArray& params) {
+__EXPORT void contour_to_contours(ParamPtrArray& params) {
 	using ContourType = std::vector<cv::Point>;
 	using ContoursType = std::vector<ContourType>;
 
@@ -191,7 +191,7 @@ EXPORT void contour_to_contours(ParamPtrArray& params) {
 *  4. <b><em>output</em></b> A `cv::Mat` object representing the output image.
 */
 
-EXPORT void warp_affine(ParamPtrArray& params) {
+__EXPORT void warp_affine(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat;
 	cv::warpAffine(get_data_const_ref<cv::Mat>(params[0]),
 		*dst,
@@ -210,7 +210,7 @@ EXPORT void warp_affine(ParamPtrArray& params) {
 *  4. <b><em>output</em></b> A `cv::Mat` object representing the rotation matrix.
 */
 
-EXPORT void get_rotation_matrix_2D(ParamPtrArray& params) {
+__EXPORT void get_rotation_matrix_2D(ParamPtrArray& params) {
 	cv::Mat* dst = new cv::Mat(cv::getRotationMatrix2D(get_data<cv::Point2f>(params[0]),
 		get_data<double>(params[1]),
 		get_data<double>(params[2])));
@@ -218,7 +218,7 @@ EXPORT void get_rotation_matrix_2D(ParamPtrArray& params) {
 	params.push_back(make_param("dst","Mat",dst));
 }
 
-EXPORT void total_pixels(ParamPtrArray& params) {
+__EXPORT void total_pixels(ParamPtrArray& params) {
 	cv::Mat src = get_data<cv::Mat>(params[0]);
 	cv::Rect rect(
 		get_data<int>(params[1]),
@@ -229,7 +229,7 @@ EXPORT void total_pixels(ParamPtrArray& params) {
 	params.push_back(make_param("total", "int", total));
 }
 
-EXPORT void mean(ParamPtrArray& params) {
+__EXPORT void mean(ParamPtrArray& params) {
 	cv::Mat src = get_data<cv::Mat>(params[0]);
 	cv::Rect rect(
 		get_data<int>(params[1]),
@@ -240,7 +240,7 @@ EXPORT void mean(ParamPtrArray& params) {
 	params.push_back(make_param("mean_value", "int", mean_value));
 }
 
-EXPORT void min_value(ParamPtrArray& params) {
+__EXPORT void min_value(ParamPtrArray& params) {
 	cv::Mat src = get_data<cv::Mat>(params[0]);
 	cv::Rect rect(
 		get_data<int>(params[1]),
@@ -253,7 +253,7 @@ EXPORT void min_value(ParamPtrArray& params) {
 	params.push_back(make_param("min_value", "double", min_value));
 }
 
-EXPORT void max_value(ParamPtrArray& params) {
+__EXPORT void max_value(ParamPtrArray& params) {
 	cv::Mat src = get_data<cv::Mat>(params[0]);
 	cv::Rect rect(
 		get_data<int>(params[1]),
@@ -264,4 +264,36 @@ EXPORT void max_value(ParamPtrArray& params) {
 	double* max_value = new double;
 	cv::minMaxLoc(src(rect), &min_value, max_value);
 	params.push_back(make_param("max_value", "double", max_value));
+}
+
+__EXPORT void flip(ParamPtrArray& params) {
+	cv::Mat* dst = new cv::Mat;
+	cv::flip(
+		get_data<cv::Mat>(params[0]), 
+		*dst, 
+		get_data<int>(params[1])
+	);
+	params.push_back(make_param("dst", "Mat", dst));
+}
+
+__EXPORT void resize(ParamPtrArray& params) {
+	cv::Mat* dst = new cv::Mat;
+	cv::resize(
+		get_data<cv::Mat>(params[0]),
+		*dst,
+		get_data<cv::Size>(params[1])
+	);
+	params.push_back(make_param("dst", "Mat", dst));
+}
+
+__EXPORT void crop(ParamPtrArray& params) {
+	cv::Mat* dst = new cv::Mat;
+	cv::Rect rect(
+		get_data<int>(params[1]),
+		get_data<int>(params[2]),
+		get_data<int>(params[3]),
+		get_data<int>(params[4])
+	);
+	get_data<cv::Mat>(params[0])(rect).copyTo(*dst);
+	params.push_back(make_param("dst", "Mat", dst));
 }
